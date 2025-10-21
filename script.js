91e55cc479e7070827a8e3cbae0a4f2d82bd593c
Mazutti-J <jose.mazutti@escola.pr.gov.br>
2025-10-21 10:14:43 -0300

    ddd

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave(){
 const texto = document.querySelector("#entrada-de-texto").value;
-const campoResultado = document.querySelector("#resultado-palavraschave");
+const campoResultado = document.querySelector("#resultado-palavrachave");
 const palavras = texto.split(" ");
 
 
