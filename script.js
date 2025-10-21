3779454b0056ec922d52f9360d6605bfc6a57d4a
Mazutti-J <jose.mazutti@escola.pr.gov.br>
2025-10-21 09:23:37 -0300

    21-10-25

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave(){
+alert("tô mal");
+
+
+
+
+
+
+
+}
